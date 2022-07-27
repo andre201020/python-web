d1b7deb18c0a1cbf9f9bf0115d2bdf710f589201
andre201020 <andrekeoch@gmail.com>
2022-07-27 00:30:14 +0000

    3a aula

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
 
+posts = [
+    {
+        "title": "1o Post",
+        "body": "texto do 1o Post",
+        "author": "autor 1o Post",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "2o Post",
+        "body": "texto do 2o Post",
+        "author": "autor 2o Post",
+        "created": datetime(2022,7,26)
+    }
+]
+
 @app.route("/")
-@app.route("/hello")
-
-def hello():
-    return "Hello Word"
-
-@app.route("/meucontato")
-def meucontato():
-    return render_template('index.html')
+def index():
+    return render_template("index.html", posts = posts)
