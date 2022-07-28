4df9067467c91c3ac3b6dd25229897d451c9bd95
andre201020 <andrekeoch@gmail.com>
2022-07-28 00:28:53 +0000

    aula 4

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("hello")
 
-posts = [
-    {
-        "title": "1o Post",
-        "body": "texto do 1o Post",
-        "author": "autor 1o Post",
-        "created": datetime(2022,7,25)
-    },
-    {
-        "title": "2o Post",
-        "body": "texto do 2o Post",
-        "author": "autor 2o Post",
-        "created": datetime(2022,7,26)
-    }
-]
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    author =db.Column(db.String(20))
+    created =db.Column(db.Datetime,nullable=False, default=datetime)
 
 @app.route("/")
 def index():
-    return render_template("index.html", posts = posts)
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts)
+
+@app.route("/populate")
+def populate():
+    post1 = Post(title="Post 1", body="Texto do Post 1", author="autor do post 1")
+    post2 = Post(title="Post 2", body="Texto do Post 2", author="autor do post 2")
+    db.session(post1)
+    db.session(post2)
+    db.session.commit()
+    return redirect("/")
