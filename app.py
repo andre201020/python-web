52fa1eb1b3a87058414f1e4b1386b422c381b820
andre201020 <andrekeoch@gmail.com>
2022-07-28 23:43:33 +0000

    aula 5...

--- app.py
+++ app.py
@@ -1,33 +1,41 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask("hello")
-
-db = SQLAlchemy(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "pudim"
 
 db = SQLAlchemy(app)
+login = LoginManager(app)
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(70), nullable=False)
     body = db.Column(db.String(500))
-    author =db.Column(db.String(20))
-    created =db.Column(db.Datetime,nullable=False, default=datetime)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-@app.route("/")
-def index():
-    posts = Post.query.all()
-    return render_template("index.html", posts=posts)
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
 
-@app.route("/populate")
-def populate():
-    post1 = Post(title="Post 1", body="Texto do Post 1", author="autor do post 1")
-    post2 = Post(title="Post 2", body="Texto do Post 2", author="autor do post 2")
-    db.session(post1)
-    db.session(post2)
-    db.session.commit()
-    return redirect("/")
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+db.create_all()
